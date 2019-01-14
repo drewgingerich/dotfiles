efba6de707f0918409d73f3cb1a1cc3b5333834d
Drew Gingerich <gingerich93@gmail.com>
2019-01-13 22:22:34 -0800

    Add Brewfile to automate app installations.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,30 @@
+#!/bin/bash
+
+# Paranoid mode. Exit on error.
+set -e
+
+# Create file to keep track of things that still need to happen after
+# this script runs.
+touch ~/todo.txt
+
+# Install Command Line Tools. Automatically accept popup window.
+xcode-select --install
+sleep 5
+osascript <<EOD
+  tell application "System Events"
+    tell process "Install Command Line Developer Tools"
+      keystroke return
+      click button "Agree" of window "License Agreement"
+    end tell
+  end tell
+EOD
+
+# Brew install.
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew bundle
+
+# Configure the world.
+
 # turn off chime at system boot
 sudo nvram SystemAudioVolume=%01
