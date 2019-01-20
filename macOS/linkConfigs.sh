f2902ff428e431c8bd5408fc53f6675187d37c32
Drew Gingerich <gingerich93@gmail.com>
2019-01-20 14:43:53 -0800

    Move config folders out of config directory. It was an unneccesary extra folder.

---
+++ macOS/linkConfigs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DOTFILES=~/configs/macOS
+
+ln -sf $DOTFILES/karabiner-elements/karabiner.json ~/.config/karabiner/karabiner.json
+ln -sf $DOTFILES/slate/.slate.js ~/.slate.js
+ln -sf $DOTFILES/bash/.bashrc ~/.bashrc
+ln -sf $DOTFILES/emacs/.emacs.d ~/.emacs.d
+ln -sf $DOTFILES/emacs/.spacemacs ~/.spacemacs
+ln -sf $DOTFILES/omnisharp/.omnisharp ~/.omnisharp
+ln -sf $DOTFILES/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -sf $DOTFILES/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sf $DOTFILES/vscode/snippets ~/Library/Application\ Support/Code/User/snippets
