f33aa972a23bd37e62bdd5dcc6e764dfc1f50237
Drew Gingerich <gingerich93@gmail.com>
2019-01-21 12:13:21 -0800

    Move setup scripts into their own folder for better organization.

---
+++ macOS/setupScripts/linkArchives.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+ARCHIVE=~/archive
+
+ln -sf $ARCHIVE/media/photos/appdata/Photos ~/Pictures/Photos\ Library.photoslibrary
+ln -sf $ARCHIVE/media/music/appdata/iTunes ~/Music/iTunes
