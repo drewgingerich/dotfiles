755d7ab0cee82d8a025a74b79046b71e1f3ec96e
Drew Gingerich <gingerich93@gmail.com>
2019-01-20 18:28:16 -0800

    Add script that configures tools. In this case, get vscode to load packages.

---
+++ macOS/configureTools.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+code --install-extension ms-python.python
+code --install-extension ms-vscode.csharp
+code --install-extension PKief.material-icon-theme
+code --install-extension Unity.unity-debug
+code --install-extension vscodevim.vim
+code --install-extension zhuangtongfa.Material-theme
