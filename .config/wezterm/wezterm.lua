91a06f95c503e15b1a3f5f94afb2134410759a5f
Drew Gingerich <drew.gingerich@ithaka.org>
2024-02-22 16:08:57 -0800

    Update Wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,6 @@ local config = {}
 config.color_scheme = 'Navy and Ivory (terminal.sexy)'
 -- config.color_scheme = 'Catppuccin Mocha'
 config.font = wezterm.font 'Inconsolata'
-config.font_size = 16
+config.font_size = 20
 
 return config
