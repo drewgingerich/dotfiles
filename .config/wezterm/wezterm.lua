c531cb679c4d8cac4d66026eff2343915e4b658f
Drew Gingerich <drew.gingerich@ithaka.org>
2024-03-04 15:15:15 -0800

    Start Wezterm maximize

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,4 +7,10 @@ config.color_scheme = 'Navy and Ivory (terminal.sexy)'
 config.font = wezterm.font 'Inconsolata'
 config.font_size = 20
 
+
+wezterm.on("gui-startup", function()
+  local tab, pane, window = wezterm.mux.spawn_window{}
+  window:gui_window():maximize()
+end)
+
 return config
