e472137ca46db48470a25f4eb4fdae18725544d4
Drew Gingerich <drew.gingerich@ithaka.org>
2024-03-06 13:11:12 -0800

    Add pane and tab navigation keybinds to wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,10 +7,41 @@ config.color_scheme = 'Navy and Ivory (terminal.sexy)'
 config.font = wezterm.font 'Inconsolata'
 config.font_size = 20
 
-
 wezterm.on("gui-startup", function()
   local tab, pane, window = wezterm.mux.spawn_window{}
   window:gui_window():maximize()
 end)
+
+config.leader = { key = 'Space', mods = 'SHIFT', timeout_milliseconds = 1000 }
+config.keys = {
+  {
+    key = 'h',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection 'Left'  
+  },
+  {
+    key = 'j',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection 'Down'  
+  },
+  {
+    key = 'k',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection 'Up'  
+  },
+  {
+    key = 'l',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection 'Right'  
+  },
+  { key = 'h',
+    mods = 'LEADER|SHIFT',
+    action = wezterm.action.ActivateTabRelative(-1)
+  },
+  { key = 'l',
+    mods = 'LEADER|SHIFT',
+    action = wezterm.action.ActivateTabRelative(1)
+  }
+}
 
 return config
