9ce355d2fbe0524b75e62e23e8a57f232bd77586
Drew Gingerich <drew.gingerich@ithaka.org>
2024-05-04 07:32:20 -0700

    nvim: add JS debugger

--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -1,31 +1,36 @@
 return {
   "mfussenegger/nvim-dap",
   dependencies = {
+    "rcarriga/nvim-dap-ui",
     "mfussenegger/nvim-dap-python",
     "leoluz/nvim-dap-go",
-    "rcarriga/nvim-dap-ui",
+    "mxsdev/nvim-dap-vscode-js",
   },
   config = function()
     require("dap-go").setup()
     require("dap-python").setup("~/.venvs/debugpy/bin/python")
+    require("dap-vscode-js").setup({
+      debugger_path = "(stdpath('data'))/mason/vscode-js-debug",
+      adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+    })
 
     local dap = require("dap")
-      local dapui = require("dapui")
+    local dapui = require("dapui")
 
-    vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, { desc = "Debugger: toggle breakpoint"})
-    vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "Debugger: continue"})
+    vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, { desc = "Debugger: toggle breakpoint" })
+    vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "Debugger: start/continue" })
 
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
   end,
 }
