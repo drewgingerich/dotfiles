90d868a14d499025c856d76d04b3fe132113624b
Drew Gingerich <drew.gingerich@ithaka.org>
2024-05-04 07:32:05 -0700

    nvim: add LuaSnip for completions

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,6 +1,11 @@
 return {
 	{
 		"hrsh7th/nvim-cmp",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp",
+    },
 		config = function()
 			local cmp = require("cmp")
 
@@ -28,9 +33,6 @@ return {
 				}),
 			})
 		end,
-	},
-	{
-		"hrsh7th/cmp-nvim-lsp",
 	},
 	{
 		"github/copilot.vim",
