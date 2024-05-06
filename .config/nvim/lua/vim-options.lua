b2a0dfc75013725ea5308504058ba8f69137330c
Drew Gingerich <drew.gingerich@ithaka.org>
2024-05-06 08:32:39 -0700

    nvim: turn off line wrapping

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -3,8 +3,10 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = false
+vim.g.loaded_netrwPlugin = false
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+vim.wo.wrap = false
