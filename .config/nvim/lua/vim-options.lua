2935b7881dfe2b76bb1ec6e1d7169618afc52e34
drewgingerich <gingerich93@gmail.com>
2024-05-19 14:53:38 -0700

    nvim: remap a few keys

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -12,8 +12,14 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  callback = function()
-    vim.opt_local.formatoptions:remove({ "o" })
-  end,
+	pattern = "*",
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "o" })
+	end,
 })
+
+vim.keymap.set("v", "<leader>y", '"*y', { desc = "Copy selection to system clipboard" })
+vim.keymap.set("n", "<leader>p", '"*p', { desc = "Paste selection from system clipboard before cursor" })
+vim.keymap.set("n", "<leader>P", '"*P', { desc = "Paste selection from system clipboard after cursor" })
+
+vim.keymap.set("n", "U", "<C-r>", { desc = "Redo change" })
