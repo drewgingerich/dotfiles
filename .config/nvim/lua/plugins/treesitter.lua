d5df24abc93a154eafb5beb0a1d304c02b83121b
Drew Gingerich <drew.gingerich@ithaka.org>
2024-08-27 11:40:48 -0700

    nvim: disable treesitter highlight for Dockerfile to avoid error

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,10 @@ return {
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
 			auto_install = true,
-			highlight = { enable = true },
+			highlight = {
+				enable = true,
+				disable = { "dockerfile" },
+			},
 			indent = { enable = true },
 		})
 	end,
