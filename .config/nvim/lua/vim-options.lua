8e44d6728fd7c1c2608ff0cabc9bbdd7fe34d5ce
drewgingerich <gingerich93@gmail.com>
2024-09-16 09:25:39 -0700

    nvim: use tabs in gdscript files

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -13,12 +13,20 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 
--- Stop commenting new line when pressing `o` or `O` while on a commented line
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "*",
-	callback = function()
-		vim.opt_local.formatoptions:remove({ "o" })
-	end,
+  desc = "Stop commenting new line when pressing `o` or `O` while on a commented line",
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "o" })
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Use tabs in GDScript files",
+  pattern = "gdscript",
+  callback = function()
+    vim.opt_local.expandtab = false
+  end,
 })
 
 vim.keymap.set("v", "<leader>y", '"*y', { desc = "Copy selection to system clipboard" })
@@ -36,6 +44,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Page up and center cursor vert
 vim.keymap.set("n", "n", "nzzzv", { desc = "Go to next match and center curor vertically" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Go to previous match and center curor vertically" })
 
-vim.keymap.set("x", "p", "\"_dP", { desc = "Paste over selection without saving to register" })
+vim.keymap.set("x", "p", '"_dP', { desc = "Paste over selection without saving to register" })
 
 vim.keymap.set("n", "<leader>s", ":%s//", { desc = "Replace search matches" })
