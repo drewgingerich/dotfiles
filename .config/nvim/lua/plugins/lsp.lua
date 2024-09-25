4d64eb67ae524ff1c29e1922f8797b46fcd50039
Drew Gingerich <drew.gingerich@ithaka.org>
2024-09-24 23:46:52 -0400

    nvim: add descriptions to more keybinds

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,26 +1,26 @@
 return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "tsserver",
-          "gopls",
-          "nil_ls",
-          "yamlls",
-          "marksman",
-        },
-      })
-    end,
-    dependencies = {
-      "williamboman/mason.nvim",
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"gopls",
+					"nil_ls",
+					"yamlls",
+					"marksman",
+				},
+			})
+		end,
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
@@ -28,50 +28,74 @@ return {
 			lspconfig.jsonls.setup({ capabilities = capabilities })
 			lspconfig.gopls.setup({ capabilities = capabilities })
 			lspconfig.pyright.setup({ capabilities = capabilities })
-      lspconfig.astro.setup({ capabilities = capabilities })
-      lspconfig.ruby_lsp.setup({ capabilities = capabilities })
-      lspconfig.marksman.setup({ capabilities = capabilities })
-      lspconfig.ruff_lsp.setup({ capabilities = capabilities })
-      lspconfig.yamlls.setup({ capabilities = capabilities })
-      lspconfig.helm_ls.setup({
-        capabilities = capabilities,
-        yamlls = { path = vim.fs.joinpath(vim.fn.stdpath("data"), "mason/bin/yaml-language-server") }
-      })
-      lspconfig.nil_ls.setup({ capabilities = capabilities })
-      lspconfig.gdscript.setup({
-        name = "godot",
-        cmd = vim.lsp.rpc.connect("127.0.0.1", "6005"),
-      })
+			lspconfig.astro.setup({ capabilities = capabilities })
+			lspconfig.ruby_lsp.setup({ capabilities = capabilities })
+			lspconfig.marksman.setup({ capabilities = capabilities })
+			lspconfig.ruff_lsp.setup({ capabilities = capabilities })
+			lspconfig.yamlls.setup({ capabilities = capabilities })
+			lspconfig.helm_ls.setup({
+				capabilities = capabilities,
+				yamlls = { path = vim.fs.joinpath(vim.fn.stdpath("data"), "mason/bin/yaml-language-server") },
+			})
+			lspconfig.nil_ls.setup({ capabilities = capabilities })
+			lspconfig.gdscript.setup({
+				name = "godot",
+				cmd = vim.lsp.rpc.connect("127.0.0.1", "6005"),
+			})
 
-      vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, { desc = "Open LSP diagnostic float" })
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
-      vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+			vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, { desc = "Open LSP diagnostic float" })
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
-      local telescope = require("telescope.builtin")
+			local telescope = require("telescope.builtin")
 
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(ev)
 					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-					local opts = { buffer = ev.buf }
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set(
+						"n",
+						"gD",
+						vim.lsp.buf.declaration,
+						{ buffer = ev.buf, desc = "Go to type declaration" }
+					)
 					vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf, desc = "Go to definition" })
 					vim.keymap.set("n", "gr", telescope.lsp_references, { buffer = ev.buf, desc = "Find references" })
-					vim.keymap.set("n", "gi", telescope.lsp_implementations, { buffer = ev.buf, desc = "Find implementations" })
-					vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, { buffer = ev.buf, desc = "Show LSP information" })
+					vim.keymap.set(
+						"n",
+						"gi",
+						telescope.lsp_implementations,
+						{ buffer = ev.buf, desc = "Find implementations" }
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>k",
+						vim.lsp.buf.hover,
+						{ buffer = ev.buf, desc = "Show LSP information" }
+					)
 					vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = ev.buf, desc = "Rename symbol" })
-					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-					vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set(
+						"n",
+						"<C-k>",
+						vim.lsp.buf.signature_help,
+						{ buffer = ev.buf, desc = "Show signature help" }
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>D",
+						vim.lsp.buf.type_definition,
+						{ buffer = ev.buf, desc = "Show type definition" }
+					)
 					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
 					vim.keymap.set("n", "<leader>f", function()
 						vim.lsp.buf.format({ async = true })
-					end, opts)
+					end, { buffer = ev.buf, desc = "Format file" })
 				end,
 			})
 		end,
-    dependencies = { "nvim-telescope/telescope.nvim" },
+		dependencies = { "nvim-telescope/telescope.nvim" },
 	},
 	{
 		"folke/trouble.nvim",
--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -14,19 +14,19 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 
 vim.api.nvim_create_autocmd("FileType", {
-  desc = "Stop commenting new line when pressing `o` or `O` while on a commented line",
-  pattern = "*",
-  callback = function()
-    vim.opt_local.formatoptions:remove({ "o" })
-  end,
+	desc = "Stop commenting new line when pressing `o` or `O` while on a commented line",
+	pattern = "*",
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "o" })
+	end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  desc = "Use tabs in GDScript files",
-  pattern = "gdscript",
-  callback = function()
-    vim.opt_local.expandtab = false
-  end,
+	desc = "Use tabs in GDScript files",
+	pattern = "gdscript",
+	callback = function()
+		vim.opt_local.expandtab = false
+	end,
 })
 
 vim.keymap.set("v", "<leader>y", '"*y', { desc = "Copy selection to system clipboard" })
@@ -41,8 +41,8 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up" }
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Page down and center cursor vertically" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Page up and center cursor vertically" })
 
-vim.keymap.set("n", "n", "nzzzv", { desc = "Go to next match and center curor vertically" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Go to previous match and center curor vertically" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Go to next match and center cursor vertically" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Go to previous match and center cursor vertically" })
 
 vim.keymap.set("x", "p", '"_dP', { desc = "Paste over selection without saving to register" })
 
